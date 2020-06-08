e094f7ab3a10e488c9f655683e0c932f0e872b77
Irina Kuranova <47595007+cryuso@users.noreply.github.com>
2020-06-08 20:12:31 +0300

    Create Z8.java

---
+++ Level_3/Z8.java
@@ -0,0 +1,20 @@
+public class Z8 {
+	public static void main(String[] args) {
+		System.out.println(lognestZero(args[0]));
+	}
+
+
+	public static String lognestZero(String s) {
+		int count = 0;
+		int temp = 0;
+		String result = "";
+		for (int i = 0; i < s.length(); i++) {
+			System.out.printf("count: %s, temp: %s, i: %s \n", count, temp, i);
+			if (Character.toString(s.charAt(i)).equals("0")) temp++;
+			else temp = 0;
+			if (temp > count) count=temp;
+		}
+		for (int i = 0; i < count; i++) result += "0";
+		return result;
+	}
+}
