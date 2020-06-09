235c29150c397af5d11dc78e18a0838c96e10975
Irina Kuranova <47595007+cryuso@users.noreply.github.com>
2020-06-09 13:27:39 +0300

    Create Z2.java

---
+++ Level_5/Z2/Z2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Task2 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.printf("Start: ");
+		String start = input.nextLine();
+		System.out.printf("End: ");
+		String end = input.nextLine();
+		System.out.println(Ferz.canMove(start, end));
+	}
+}
